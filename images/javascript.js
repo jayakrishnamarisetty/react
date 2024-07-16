89f991307c61803bd9584722e2b6aeceebfdafad
jayakrishnamarisetty <22A91A61G8@aec.edu.in>
2024-07-16 10:21:11 +0530

    frist commit

---
+++ images/javascript.js
@@ -0,0 +1,64 @@
+// x=10
+// y=11
+// if(x > y) console.log(x)
+// else console.log(y)    
+
+// var a=[9,10,11,12,13,14,15]
+// a.map(jk =>{
+//     console.log(jk)
+// })
+
+
+
+
+// var jk=[1,2,3,5,7,6,8,9]
+// var rug=0
+// for(var i=0;i<jk.length;i++){
+//     if (arr[i] % 2 != 0) 
+//     {  
+//             rug++;  
+//             if (rug == 3) 
+//             { 
+//                 return true;
+//             }
+//     } 
+//             else 
+//             {  
+//                 rug = 0;
+//             }
+// }console.log("False") 
+
+// let n = 29; 
+// let rug = true;
+
+// if (n <= 1) {
+//     rug = false;
+// } else if (n <= 3) {
+//     rug = true;
+// } else if (n % 2 === 0 || n % 3 === 0) {
+//     rug = false;
+// } else {
+//     for (let i = 5; i * i <= n; i += 6) {
+//         if (rug % i === 0 || n % (i + 2) === 0) {
+//             rug = false;
+//             break;
+//         }
+//     }
+// }
+var x= new Date();
+document.write((x.getDate())+ "-" + (1+x.getMonth()) + "-" + (x.getFullYear()))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.3))
+console.log(Math.floor(4.3))
+console.log(Math.trunc (4.3))
+console.log(Math.pow(4,3))
+console.log(Math.sqrt(4))
+console.log(50 + Math.floor(Math.round()*50))
+function jk(a,b,c){
+    console.log(a+b)
+}
+jk(2,4)
+
+
+
+
